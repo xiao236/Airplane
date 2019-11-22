319aae5de4b891c0e1dbb7c84610fdf1737ac1d4
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2019-11-22 15:15:53 -0500

    words

--- src/ReservationClient.java
+++ src/ReservationClient.java
@@ -10,8 +10,16 @@ public class ReservationClient {
                 "Passengers who use T-Mobile as a cell phone carrier get additional benefits.\n" +
                 "We are also happy to offer power outlets in each seat for passenger use.\n" +
                 "We hope you choose to fly Delta as your next Airline.";
-        String SouthwestAd = "Southwest Airline";
-        String AlaskaAd = "Alaska Airline";
+        String SouthwestAd = "Southwest Airlines is proud to offer flights to Purdue University.\n" +
+                "We are happy to offer free in flight wifi, as well as our amazing snacks.\n" +
+                "In addition, we offer flights for much cheaper than other airlines, and offer two free checked bags\n" +
+                "We hope you choose Southwest for your next flight\n";
+        String AlaskaAd = "Alaska Airlines is proud to serve the strong and knowledgeable Boilermakers from Purdue University.\n" +
+                "We primarily fly westward, and often have stops in Alaska and California.\n" +
+                "We have first class amneties, even in coach class.\n" +
+                "We provide fun snacks, such as pretzels and goldfish.\n" +
+                "We also have comfortable seats and free WiFi.\n" +
+                "We hope you choose Alaska Airlines for your next itinerary.\n";
         String[] options1 = {
                 "Exit", "Book a Flight"
         };
