fdde8187cf4696c6546c2e0108c0b04f7ccd3a3b
noah1320 <54676274+noah1320@users.noreply.github.com>
2019-11-22 12:59:29 -0600

    changes

--- src/Alaska.java
+++ src/Alaska.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Alaska implements Airline {
+    public Alaska(){
+
+    }
+
+    public String[] readClient(){
+        String[] a = new String[];
+
+        return a;
+    }
 }
--- src/ReservationServer.java
+++ src/ReservationServer.java
@@ -1,2 +1,30 @@
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+
 public class ReservationServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4242);
+        Socket socket = serverSocket.accept();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        writer.flush();
+
+        while(true){
+            String a= reader.readLine();
+
+            switch (a){
+                case "Alaska":
+                    Alaska();
+                    break;
+                case "Delta":
+                    break;
+                case "Southwest":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 }
