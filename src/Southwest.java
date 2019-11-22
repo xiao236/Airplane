d126aed2b853d7e922a1c2a3b0442e6528fcf304
noah1320 <54676274+noah1320@users.noreply.github.com>
2019-11-22 13:39:41 -0600

    Update Southwest.java

--- src/Southwest.java
+++ src/Southwest.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Southwest implements Airline {
+    @Override
+    public String[] readClient() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("reservation.txt"));
+        ArrayList<String> listOfpassenger= new ArrayList<>();
+        int a=0;
+        String b;
+
+        while ((b = br.readLine()) != null){
+            if(b.equals("Southwest passenger list")){
+                break;
+            }
+            if(a==1) {
+                listOfpassenger.add(b);
+            }
+            if(b.equals("Southwest")){
+                a++;
+            }
+        }
+        int sizeofarray = listOfpassenger.size();
+        String[] arrayofpassenger = new String[sizeofarray];
+        a=0;
+        for(String k:listOfpassenger){
+            arrayofpassenger[a]=k;
+            a++;
+        }
+        return arrayofpassenger;
+    }
 }
