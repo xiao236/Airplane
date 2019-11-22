5fb0b276aa1727f61004b73aa21b663654422794
noah1320 <54676274+noah1320@users.noreply.github.com>
2019-11-22 13:38:52 -0600

    summary

--- src/Alaska.java
+++ src/Alaska.java
@@ -1,13 +1,32 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class Alaska implements Airline {
-    public Alaska(){
 
-    }
+    public String[] readClient() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("reservation.txt"));
+        ArrayList<String> listOfpassenger= new ArrayList<>();
+        int a=0;
+        String b;
 
-    public String[] readClient(){
-        String[] a = new String[];
-
-        return a;
+        while ((b = br.readLine()) != null){
+            if(b.equals("Alaska passenger list")){
+                break;
+            }
+            if(a==1) {
+                listOfpassenger.add(b);
+            }
+            if(b.equals("Alaska")){
+                a++;
+            }
+        }
+        int sizeofarray = listOfpassenger.size();
+        String[] arrayofpassenger = new String[sizeofarray];
+        a=0;
+        for(String k:listOfpassenger){
+            arrayofpassenger[a]=k;
+            a++;
+        }
+        return arrayofpassenger;
     }
 }
