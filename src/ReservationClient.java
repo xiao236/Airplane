2b7f579ed12337a41941ec3ceef11467b5a5a33b
noah1320 <54676274+noah1320@users.noreply.github.com>
2019-11-22 14:08:16 -0600

    changes to gui

--- src/ReservationClient.java
+++ src/ReservationClient.java
@@ -1,15 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ReservationClient {
     public static void main(String args[]) {
-        String DeltaAd = "Delta Airlines is proud to be one of the five premier Airlines at Purdue University." +
-                "We are extremely happy to offer exceptional services, with free limited WiFi for all customers." +
-                "Passengers who use T-Mobile as a cell phone carrier get additional benefits." +
-                "We are also happy to offer power outlets in each seat for passenger use." +
+        String DeltaAd = "Delta Airlines is proud to be one of the five premier Airlines at Purdue University.\n" +
+                "We are extremely happy to offer exceptional services, with free limited WiFi for all customers.\n" +
+                "Passengers who use T-Mobile as a cell phone carrier get additional benefits.\n" +
+                "We are also happy to offer power outlets in each seat for passenger use.\n" +
                 "We hope you choose to fly Delta as your next Airline.";
         String SouthwestAd = "Southwest Airline";
+        String AlaskaAd = "Alaska Airline";
         String[] options1 = {
                 "Exit", "Book a Flight"
         };
@@ -33,6 +35,20 @@ public class ReservationClient {
             jf.setSize(900, 900);
             jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             JComboBox<String> airlineList = new JComboBox<>(airlines);
+            JPanel panel1 = new JPanel();
+            panel1.setSize(900,100);
+            JPanel panel2= new JPanel();
+            panel2.setSize(900,500);
+            JPanel panel3= new JPanel();
+            JLabel jldelta = new JLabel(DeltaAd);
+            JLabel jlsouth = new JLabel(SouthwestAd);
+            JLabel jlalaska = new JLabel(AlaskaAd);
+            jldelta.setVisible(false);
+            jlsouth.setVisible(false);
+            jlalaska.setVisible(false);
+            panel2.add(jldelta);
+            panel2.add(jlsouth);
+            panel2.add(jlalaska);
             airlineList.setSelectedIndex(0);
             airlineList.addActionListener(new ActionListener() {
                 @Override
@@ -41,12 +57,18 @@ public class ReservationClient {
                     String airline = (String)cb.getSelectedItem();
                     assert airline != null;
                     switch (airline) {
-                        case "Delta": jf.add(new JLabel(DeltaAd)); break;
-                        case "Southwest": jf.add(new JLabel(SouthwestAd)); break;
+                        case "Delta": jldelta.setVisible(true); jlsouth.setVisible(false);jlalaska.setVisible(false); break;
+                        case "Southwest": jlsouth.setVisible(true);jldelta.setVisible(false);jlalaska.setVisible(false); break;
+                        case "Alaska": jlalaska.setVisible(true);jldelta.setVisible(false);jlsouth.setVisible(false);break;
                     }
                 }
             });
-            jf.add(airlineList);
+
+            panel1.add(airlineList);
+            jf.add(panel1,BorderLayout.NORTH);
+            jf.add(panel2,BorderLayout.CENTER);
+            jf.add(panel3,BorderLayout.SOUTH);
+
             jf.setVisible(true);
             //airline = (String) JOptionPane.showInputDialog(null, "Choose a flight from the drop down menu.", "Purdue University Flight Reservation System", JOptionPane.QUESTION_MESSAGE, null, airlines, airlines[0]);
         }
