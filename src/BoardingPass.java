ea012940314f6c9c0ff2f253348c60595589bc9b
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2019-11-22 14:49:26 -0500

    basic constructor stuff

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,4 +1,17 @@
 import java.io.Serializable;
 
 public class BoardingPass implements Serializable {
+    private String airline;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gate gate;
+
+    public BoardingPass(String airline, String firstName, String lastName, int age, Gate gate) {
+        this.airline = airline;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gate = gate;
+    }
 }
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,5 +1,17 @@
 import java.io.Serializable;
 
 public class Passenger implements Serializable {
+    private String firstName;
+    private String lastName;
+    private BoardingPass bp;
+    private int age;
+    public Passenger(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
+    public void setBoardingPass(BoardingPass bp) {
+        this.bp = bp;
+    }
 }
