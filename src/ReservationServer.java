063c2f82afc2b54455bda5d5ab48b581b05f3bf1
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2019-12-02 14:15:24 -0500

    reading the file

--- src/ReservationServer.java
+++ src/ReservationServer.java
@@ -5,6 +5,36 @@ import java.net.*;
 public class ReservationServer {
     public static void main(String[] args) throws IOException {
         String[] listofpassenger=null;
+        Alaska a = new Alaska();
+        Delta d = new Delta();
+        Southwest s = new Southwest();
+
+        BufferedReader br = new BufferedReader(new FileReader("reservations.txt"));
+        int[] howManyPassengers = new int[2];
+        br.readLine();
+        String next = br.readLine();
+        String[] hold = next.split("/");
+        for (int i = 0; i < 2; i ++) {
+            howManyPassengers[i] = Integer.parseInt(hold[i]);
+        }
+        br.readLine();
+        br.readLine();
+        for (int i = 0; i < howManyPassengers[0]; i++) {
+            next = br.readLine();
+            hold = next.split(" ");
+            Passenger p = new Passenger(hold[0], hold[1].substring(0, hold[1].length()-1), Integer.parseInt(hold[2]));
+            a.addPassenger(p);
+            br.readLine();
+        }
+        br.readLine();
+        br.readLine();
+        next = br.readLine();
+        hold = next.split("/");
+        for (int i = 0; i < 2; i ++) {
+            howManyPassengers[i] = Integer.parseInt(hold[i]);
+        }
+
+
         ServerSocket serverSocket = new ServerSocket(4242);
         Socket socket = serverSocket.accept();
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -12,9 +42,9 @@ public class ReservationServer {
         writer.flush();
 
         while(true){
-            String a= reader.readLine();
+            String yes = reader.readLine();
 
-            switch (a){
+            switch (yes){
                 case "Alaska":
                     Alaska al =new Alaska();
                     listofpassenger= al.readClient();
