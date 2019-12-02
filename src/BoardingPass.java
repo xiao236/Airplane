4349cf57f93559cc3628df2c221435790ca20d63
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2019-12-02 18:08:54 -0500

    yep

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -6,6 +6,7 @@ public class BoardingPass implements Serializable {
     private String lastName;
     private int age;
     private Gate gate;
+    private int flightNumber;
 
     public BoardingPass(String airline, String firstName, String lastName, int age, Gate gate) {
         this.airline = airline;
@@ -13,9 +14,15 @@ public class BoardingPass implements Serializable {
         this.lastName = lastName;
         this.age = age;
         this.gate = gate;
+        switch(airline) {
+            case "Delta": flightNumber = 20000; break;
+            case "Alaska": flightNumber = 18000; break;
+            case "Southwest": flightNumber = 22000; break;
+            default: flightNumber = 16000; break;
+        }
     }
     public String toString() {
-        return "<HTML>BOARDING PASS FOR FLIGHT 18000 WITH " + airline + " Airlines<br/>PASSENGER FIRST NAME: " +
+        return "<HTML>BOARDING PASS FOR FLIGHT " + flightNumber + " WITH " + airline + " Airlines<br/>PASSENGER FIRST NAME: " +
                 firstName + "<br/>PASSENGER LAST NAME: " + lastName + "<br/>PASSENGER AGE: " + age +
                 "<br/>You can now begin boarding at gate " + gate+"<HTML>";
     }
