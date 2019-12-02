0429c3554dd298a2ecf00c76c706794eb57a8055
noah1320 <54676274+noah1320@users.noreply.github.com>
2019-12-02 13:53:24 -0600

    adding buttons

--- src/ReservationClient.java
+++ src/ReservationClient.java
@@ -7,6 +7,48 @@ import java.net.Socket;
 
 public class ReservationClient {
     public static void main(String args[]) throws IOException {
+        String[] options1 = {
+                "Exit", "Book a Flight"
+        };
+        String[] options2 = {
+                "Exit", "Yes, I want to book a flight"
+        };
+        ImageIcon icon = new ImageIcon("purdueLogo.png");
+        while (true) {
+            String hostName = JOptionPane.showInputDialog(null, "What is the hostname you'd like to connect to?", "Hostname?", JOptionPane.QUESTION_MESSAGE);
+            if (hostName == null) {
+                break;
+            }
+            String port = JOptionPane.showInputDialog(null, "What is the port you'd like to connect to?", "Port?", JOptionPane.QUESTION_MESSAGE);
+            if (port == null) {
+                break;
+            }
+            //Socket socket = new Socket(hostName, Integer.parseInt(port));
+
+            int choice = JOptionPane.showOptionDialog(null, "", "Purdue University Flight Reservation System", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, icon, options1, options1[0]);
+
+            if (choice != 0) {
+                choice = JOptionPane.showOptionDialog(null, "Do you want to book a flight today?", "Purdue University Flight Reservation System", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options2, options2[0]);
+            } else {
+                break;
+            }
+            if (choice != 0) {
+                createAndShowGUI();
+                //airline = (String) JOptionPane.showInputDialog(null, "Choose a flight from the drop down menu.", "Purdue University Flight Reservation System", JOptionPane.QUESTION_MESSAGE, null, airlines, airlines[0]);
+            } else {
+                break;
+            }
+            break;
+        }
+        //JOptionPane.showMessageDialog(null,"Thank you for using the Purdue University Airline Management System!");
+    }
+
+
+    private static void createAndShowGUI() {
+        String[] airlines = {
+                "Delta", "Alaska", "Southwest"
+        };
+
         String DeltaAd = "<html>Delta Airlines is proud to be one of the five premier Airlines at Purdue University.<br/>" +
                 "We are extremely happy to offer exceptional services, with free limited WiFi for all customers.<br/>" +
                 "Passengers who use T-Mobile as a cell phone carrier get additional benefits.<br/>" +
@@ -22,94 +64,71 @@ public class ReservationClient {
                 "We provide fun snacks, such as pretzels and goldfish.<br/>" +
                 "We also have comfortable seats and free WiFi.<br/>" +
                 "We hope you choose Alaska Airlines for your next itinerary.</html>";
-        String[] options1 = {
-                "Exit", "Book a Flight"
-        };
-        String[] options2 = {
-                "Exit", "Yes, I want to book a flight"
-        };
-        String[] airlines = {
-                "Delta", "Alaska", "Southwest"
-        };
-        ImageIcon icon = new ImageIcon("purdueLogo.png");
-        while(true) {
-            String hostName = JOptionPane.showInputDialog(null, "What is the hostname you'd like to connect to?", "Hostname?", JOptionPane.QUESTION_MESSAGE);
-            if(hostName==null){
-                break;
+        JFrame jf = new JFrame("Purdue University Flight Reservation System");
+        jf.setSize(900, 900);
+        jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JComboBox<String> airlineList = new JComboBox<>(airlines);
+        JPanel panel1 = new JPanel();
+        panel1.setPreferredSize(new Dimension(900, 100));
+        JPanel panel2 = new JPanel();
+        panel2.setSize(900, 500);
+        JPanel panel3 = new JPanel();
+        JLabel jldelta = new JLabel(DeltaAd);
+        JLabel jlsouth = new JLabel(SouthwestAd);
+        JLabel jlalaska = new JLabel(AlaskaAd);
+        jldelta.setVisible(false);
+        jlsouth.setVisible(false);
+        jlalaska.setVisible(false);
+        panel2.add(jldelta);
+        panel2.add(jlsouth);
+        panel2.add(jlalaska);
+        airlineList.setSelectedIndex(0);
+        airlineList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JComboBox cb = (JComboBox) actionEvent.getSource();
+                String airline = (String) cb.getSelectedItem();
+                assert airline != null;
+                switch (airline) {
+                    case "Delta":
+                        jldelta.setVisible(true);
+                        jlsouth.setVisible(false);
+                        jlalaska.setVisible(false);
+                        break;
+                    case "Southwest":
+                        jlsouth.setVisible(true);
+                        jldelta.setVisible(false);
+                        jlalaska.setVisible(false);
+                        break;
+                    case "Alaska":
+                        jlalaska.setVisible(true);
+                        jldelta.setVisible(false);
+                        jlsouth.setVisible(false);
+                        break;
+                }
             }
-            String port = JOptionPane.showInputDialog(null, "What is the port you'd like to connect to?", "Port?", JOptionPane.QUESTION_MESSAGE);
-            if(port==null){
-                break;
-            }
-            //Socket socket = new Socket(hostName, Integer.parseInt(port));
+        });
+        JLabel choose = new JLabel("Choose a flight from the drop down menu\n");
+        choose.setFont(choose.getFont().deriveFont(32.0f));
+        panel1.add(choose);
+        panel1.add(airlineList);
+        jf.add(panel1, BorderLayout.NORTH);
+        jf.add(panel2, BorderLayout.CENTER);
+        jf.add(panel3, BorderLayout.SOUTH);
+        JButton button1 = new JButton("Exit");
+        JButton button2 = new JButton("Choose this flight");
 
-            int choice = JOptionPane.showOptionDialog(null, "", "Purdue University Flight Reservation System", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, icon, options1, options1[0]);
+        //set action listeners for buttons
+        button1.addActionListener(this);
+        button2.addActionListener(this);
 
-            if (choice != 0) {
-                choice = JOptionPane.showOptionDialog(null, "Do you want to book a flight today?", "Purdue University Flight Reservation System", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options2, options2[0]);
-            }else{
-                break;
-            }
-            if (choice != 0) {
-                JFrame jf = new JFrame("Purdue University Flight Reservation System");
-                jf.setSize(900, 900);
-                jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                JComboBox<String> airlineList = new JComboBox<>(airlines);
-                JPanel panel1 = new JPanel();
-                panel1.setPreferredSize(new Dimension(900, 100));
-                JPanel panel2 = new JPanel();
-                panel2.setSize(900, 500);
-                JPanel panel3 = new JPanel();
-                JLabel jldelta = new JLabel(DeltaAd);
-                JLabel jlsouth = new JLabel(SouthwestAd);
-                JLabel jlalaska = new JLabel(AlaskaAd);
-                jldelta.setVisible(false);
-                jlsouth.setVisible(false);
-                jlalaska.setVisible(false);
-                panel2.add(jldelta);
-                panel2.add(jlsouth);
-                panel2.add(jlalaska);
-                airlineList.setSelectedIndex(0);
-                airlineList.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent actionEvent) {
-                        JComboBox cb = (JComboBox) actionEvent.getSource();
-                        String airline = (String) cb.getSelectedItem();
-                        assert airline != null;
-                        switch (airline) {
-                            case "Delta":
-                                jldelta.setVisible(true);
-                                jlsouth.setVisible(false);
-                                jlalaska.setVisible(false);
-                                break;
-                            case "Southwest":
-                                jlsouth.setVisible(true);
-                                jldelta.setVisible(false);
-                                jlalaska.setVisible(false);
-                                break;
-                            case "Alaska":
-                                jlalaska.setVisible(true);
-                                jldelta.setVisible(false);
-                                jlsouth.setVisible(false);
-                                break;
-                        }
-                    }
-                });
-                JLabel choose = new JLabel("Choose a flight from the drop down menu\n");
-                choose.setFont (choose.getFont ().deriveFont (32.0f));
-                panel1.add(choose);
-                panel1.add(airlineList);
-                jf.add(panel1, BorderLayout.NORTH);
-                jf.add(panel2, BorderLayout.CENTER);
-                jf.add(panel3, BorderLayout.SOUTH);
-                
-                jf.setVisible(true);
-                //airline = (String) JOptionPane.showInputDialog(null, "Choose a flight from the drop down menu.", "Purdue University Flight Reservation System", JOptionPane.QUESTION_MESSAGE, null, airlines, airlines[0]);
-            }else{
-                break;
-            }
-            break;
-        }
-        //JOptionPane.showMessageDialog(null,"Thank you for using the Purdue University Airline Management System!");
+        //add buttons to the frame
+        add(button1);
+        add(button2);
+        jf.setVisible(true);
+    }
+
+    public void setButton(JPanel){
+
     }
 }
