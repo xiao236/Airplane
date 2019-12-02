0baf2a89881d1aa4995c51fcc80a0ef6868d132c
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2019-12-02 16:47:25 -0500

    gate update

--- src/Gate.java
+++ src/Gate.java
@@ -1,10 +1,17 @@
 import java.io.Serializable;
+import java.util.Random;
 
 public class Gate implements Serializable {
-    private char terminal;
+    private String terminal;
     private int gate;
-    public Gate(char terminal, int gate) {
-        this.terminal = terminal;
-        this.gate = gate;
+    public Gate() {
+        Random rand = new Random();
+        int term = rand.nextInt(3);
+        gate = 1 + rand.nextInt(18);
+        switch(term) {
+            case 0: terminal = "A"; break;
+            case 1: terminal = "B"; break;
+            case 2: terminal = "C"; break;
+        }
     }
 }
